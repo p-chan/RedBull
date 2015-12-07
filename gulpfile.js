ae4078cf63e0abfacbb14c5558ab73868c627c2c
p1ch_jp <p1ch.jp@gmail.com>
2015-12-08 01:04:10 +0900

    :sparkles: Add task of js (ES6 compile)

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,9 @@ var gulpHandlebars = require('gulp-handlebars-html')(handlebars);
 var rename = require('gulp-rename');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
 
 var browserSync = require('browser-sync');
 
@@ -49,6 +52,23 @@ gulp.task('stylus', () => {
     .pipe(browserSync.stream());
 });
 
+gulp.task('js', () => {
+  browserify({
+    entries: './app/js/app.js',
+    extensions: '.js'
+  })
+    .transform(babelify)
+    .bundle()
+    .on('error', function () {
+      var args = Array.prototype.slice.call(arguments);
+      notify.onError('js: <%= error.message %>').apply(this, args);
+      this.emit('end');
+    })
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/assets/js'))
+    .pipe(browserSync.stream());
+})
+
 gulp.task('browser-sync', () => {
   browserSync({
     server: {
@@ -57,7 +77,8 @@ gulp.task('browser-sync', () => {
   });
 });
 
-gulp.task('default', ['handlebars', 'stylus', 'browser-sync'], () => {
+gulp.task('default', ['handlebars', 'stylus', 'js', 'browser-sync'], () => {
   gulp.watch('./app/view/**/*.hbs', ['handlebars']);
   gulp.watch('./app/stylus/**/*.styl', ['stylus']);
+  gulp.watch('./app/js/**/*.js', ['js']);
 });
